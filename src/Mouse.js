b934c06f876d427b988aad674b8949399d29d917
EvgenPrushk <ogmprushak@mail.ru>
2021-03-05 15:13:11 +0300

    feat: add mouse

--- src/Mouse.js
+++ src/Mouse.js
@@ -1,13 +1,24 @@
 class Mouse {
   // находиться ли мышка над элементом
+  element = null;
   under = false;
   pUnder = false;
-    // координаты мыши
+  // координаты мыши
   x = null;
   y = null;
 
   pX = null;
   pY = null;
+  // прожата ли левая клавиша мыши
+  left = false;
+  pLeft = false;
+  // прожата ли колесико клавиша мыши
+  delta = false;
+  pDelta = false;
+
+  constructor(element) {
+    this.element = element;
+    element.add
+  }
   
-  constructor(element) {}
 }
