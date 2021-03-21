b29978d16c6c9561193447b2644d90964950fe8f
EvgenPrushk <ogmprushak@mail.ru>
2021-03-22 01:14:29 +0300

    feat: add class Shot and ShotView

--- src/scenes/ComputerScene.js
+++ src/scenes/ComputerScene.js
@@ -8,4 +8,19 @@ class ComputerScene extends Scene {
       .querySelector('[data-scene="computer"]')
       .classList.remove("hidden");
   }
+
+  update() {
+    // забираем с помощью диструктуризации  данные
+    const { mouse, opponent, player } = this.app;
+    // т.к cells - это матрица, то мы ее схлопываем 1 уровень вложенности за счет
+    // flat()
+    const cells = opponent.cells.flat();
+    const cell = cells.find((cell) => isUnderPoint(mouse, cell));
+
+    cells.forEach((cell) => cell.classList.remove("battlefield-item__active"));
+
+    if (isUnderPoint(mouse, opponent.table)) {
+      cell.classList.add("battlefield-item__active");
+    }
+  }
 }
